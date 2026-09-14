2f69b9282be903be9ab25182537f1ab36cb58eff
JorgeV92 <jorgevmg31@gmail.com>
2026-09-13 22:07:52 -0500

    KNN

--- knn.py
+++ knn.py
@@ -12,3 +12,39 @@ yte = np.array(test_dataset.targets)
 
 print(Xtr.shape); print(ytr.shape) 
 print(Xte.shape); print(yte.shape)
+
+Xtr = Xtr.reshape(Xtr.shape[0], -1)
+Xte = Xte.reshape(Xtr.shape[0], -1)
+
+Xtr = Xtr.astype(np.float32)
+Xte = Xte.astype(np.float32)
+
+print(Xtr.shape)
+
+Xtr = Xtr[:5000]
+ytr = ytr[:5000]
+
+Xte = Xte[:500]
+yte = yte[:500]
+
+class KNN:
+    def __init__(self) -> None:
+        self.Xtr = None 
+        self.ytr = None 
+
+    def fit(self, X, y):
+        pass 
+
+    def compute_distances(self, X):
+        pass 
+
+    def pred(self, X, k=1):
+        pass 
+
+    def pred_labels(self, dist, k=1):
+        pass 
+
+
+
+
+
