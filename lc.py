5f2fb1dab509fc595b4dda88569a94569265906d
JorgeV92 <jorgevmg31@gmail.com>
2026-09-15 18:52:57 -0500

    softmax

--- lc.py
+++ lc.py
@@ -18,4 +18,20 @@ def svm_loss(W, b, X, y, reg):
     dW += 2 * reg * W 
     return loss, dW, db 
 
-    
+def softmax_loss(W, b, X, y, reg):
+    N = X.shape[0]
+    scores = X @ W + b
+    scores -= np.max(scores, axis=1, keepdims=True) 
+    exp_scores = np.exp(scores)
+    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+    corect_probs = probs[np.arange(N), y]
+    loss = -np.mean(np.log(corect_probs + 1e-12))
+    loss += reg * np.sum(W*W)
+    # Gradient 
+    dscores = probs.copy()
+    dscores[np.arange(N), y] -= 1
+    dscores /= N 
+    dW = X.T @ dscores
+    db = dscores.sum(axis=0)
+    dW += 2 * reg * W 
+    return loss, dW, db
