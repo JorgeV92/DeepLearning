6cdc01f79cc1a36cbad99929819fae979f71e6b0
JorgeV92 <jorgevmg31@gmail.com>
2026-09-15 19:36:00 -0500

    Linear classifier

--- lc.py
+++ lc.py
@@ -35,3 +35,34 @@ def softmax_loss(W, b, X, y, reg):
     db = dscores.sum(axis=0)
     dW += 2 * reg * W 
     return loss, dW, db
+
+
+class LinearClassifier:
+    def __init__(self, input_dim, num_classes, loss_fn):
+        self.loss_fn = loss_fn 
+        rng = np.random.default_rng(42)
+        self.W = (0.001 * rng.standard_normal((input_dim, num_classes))).astype(np.float32)
+        self.b = np.zeros(num_classes, dtype=np.float32)
+
+    def train(self, X, y, learning_rate=1e-2, reg=1e-4, num_iters=1000, batch_size=256):
+        rng = np.random.default_rng(42)
+        N = X.shape[0]
+        for it in range(num_iters):
+            indices = rng.choice(N, size=batch_size,replace=False)
+            X_batch = X[indices]
+            y_batch = y[indices]
+            loss, dW, db = self.loss_fn( self.W, self.b, X_batch, y_batch, reg)
+            # SGD update
+            self.W -= learning_rate * dW
+            self.b -= learning_rate * db
+            if it % 100 == 0:
+                print(
+                    f"iteration {it:4d} "
+                    f"loss = {loss:.4f}")
+    def pred(self, X):
+        scores = X @ self.W + self.b 
+
+    def accuracy(self, X, y):
+        predictions = self.pred(X)
+        return np.mean(predictions == y)
+
